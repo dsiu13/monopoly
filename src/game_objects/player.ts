b1842aa5d76669a9118085f246129ec6df2605f6
dsiu13 <dereksiu216@gmail.com>
2026-09-25 17:43:31 -0700

    added more to player

--- src/game_objects/player.ts
+++ src/game_objects/player.ts
@@ -1,9 +1,15 @@
-class Player {
-    name: string;
-    capital: number = 1500;
+import {PropertyTile} from './PropertyTile';
 
-    constructor(name: string){
+export class Player {
+    name: string;
+    token: string;
+    position: number = 0;
+    capital: number = 1500;
+    properties: PropertyTile[] = [];
+
+    constructor(name: string, token: string){
         this.name = name;
+        this.token = token;
     }
 
     payAmount(cost: number): boolean {
@@ -17,5 +23,14 @@ class Player {
 
     gainCapital(amount: number): void {
         this.capital += amount;
+    }
+
+    purchaseProperty(deed: PropertyTile): boolean {
+        if (!this.payAmount(deed.price)) {
+            return false;
+        }
+        
+        this.properties.push(deed)
+
     }
 }
---
+++ src/game_objects/property_tile.ts
@@ -0,0 +1,25 @@
+import {Player} from './player';
+
+export class PropertyTile {
+    owner: string;
+    name: string;
+    price: number;
+    color: string;
+    rent: number;
+    houses: number;
+    hotels: number;
+
+
+constructor (
+    owner: string,
+    name: string, 
+    price: number, 
+    color: string, 
+    rent: number,
+    houses: number,
+    hotels: number
+
+)
+
+}
+
